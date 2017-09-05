b411e00a8067d948bcff6c37e257fefa1a4fb38e
buddha314 <buddha_314@yahoo.com>
2017-09-05 12:34:54 -0700

    quick update to python sample of cosim

--- test/reference/cosim_reference.py
+++ test/reference/cosim_reference.py
@@ -1,5 +1,8 @@
 """
   Here to check the performance of calculation of cosine similarity
+  Here is an SO on the topic in Python: https://stackoverflow.com/questions/17627219/whats-the-fastest-way-in-python-to-calculate-cosine-similarity-given-sparse-mat
+
+  And it points out I want 1-cs, duh... :)
 """
 import numpy as np
 import scipy
